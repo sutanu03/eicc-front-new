2d4ce098fa99c3f6be5e84d40ae3e9e8957bc80b
sutanu03 <csesutanu@gmail.com>
2024-09-12 13:53:35 +0530

    position changed for sticky to auto

--- app/DomesticHelp/page.js
+++ app/DomesticHelp/page.js
@@ -28,6 +28,7 @@ const Domestic = () => {
   return (
     <>
     <Header />
+    <div className='overflow-auto'>
       <div className="d-flex flex-column flex-md-row justify-content-center align-items-center gap-3 mt-5">
         <div className={`tpn_card ${selectedItems.includes('Brooming') ? 'selected' : ''}`} onClick={() => toggleSelection('Brooming')}>
           <img src="/img/brooming.png" className="w-100 mb-4" />
@@ -69,6 +70,7 @@ const Domestic = () => {
           </Button>
         </Link>
       </div>
+    </div>
     </>
   );
 };
